5e62cfd506a406b81bac5c5047cec946c20f1485
neogeoss <neogeo-s@hanmail.net>
2019-09-02 01:16:54 +0900

    Single quotation mark changed to apostrophes to show PORT number correctly

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) =>{
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log('Server is running on port ${port}');
+    console.log(`Server is running on port ${port}`);
 });
 
