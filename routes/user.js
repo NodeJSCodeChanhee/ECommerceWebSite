7c6ab8c7748c13b5acb5f761e6ca940f55d1e1cf
neogeoss <neogeo-s@hanmail.net>
2019-09-03 00:19:46 +0900

    controller added

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello from node user routes");
-});
+const {sayHi} = require('../controllers/user');
+
+router.get("/", sayHi);
 
 module.exports = router;
