46bc47561929bb0b88bfece495ab6426024e57c5
neogeoss <neogeo-s@hanmail.net>
2019-09-03 00:05:25 +0900

    MongoDB connection

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+//app
 const app = express();
 require('dotenv').config();
+
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser : true,
+    useCreateIndex : true
+}).then(()=> console.log("DB connected"));
 
 app.get('/', (req, res) =>{
     res.send('Hello from node updated hahah hohoho');
